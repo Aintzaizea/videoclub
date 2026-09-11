053cebb8a297c5345980833e95465f1cbb97b72e
Aintzaizea <neithe@gmail.com>
2026-09-11 11:01:02 +0200

    modificado json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { readFile, writeFile } from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,6 +9,7 @@ const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const archivoPeliculas = path.join(__dirname, 'peliculas.json');
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,38 +19,42 @@ app.use('/img', express.static(path.join(__dirname, 'img')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-let peliculas = [
-{ id: Date.now(), titulo: 'El Padrino', director: 'Francis Ford Coppola', anio: 1972 },
-{ id: Date.now() + 1, titulo: 'Pulp Fiction', director: 'Quentin Tarantino', anio: 1994 },
-{ id: Date.now() + 2, titulo: 'Parásitos', director: 'Bong Joon-ho', anio: 2019 }
-];
+
+let peliculas = JSON.parse(await readFile(archivoPeliculas, 'utf-8'));
+
+async function guardarPeliculas() {
+  await writeFile(archivoPeliculas, JSON.stringify(peliculas, null, 2));
+}
 
 
 app.get('/api/peliculas', (req, res) => {
     res.json(peliculas);
 });
 
-app.post('/anadir-pelicula', (req, res) => {
+app.post('/anadir-pelicula', async (req, res) => {
   req.body.id = Date.now();
   req.body.anio = Number(req.body.anio)
   peliculas.push(req.body);
+  await guardarPeliculas();
   console.log(req.body);
   res.redirect('/');
 });
 
-app.post ("/editar-pelicula", (req, res)=> {
+app.post ("/editar-pelicula", async (req, res)=> {
 const idPeliculaEditar = Number(req.body.id);
 const pelicula = peliculas.find(p => p.id === idPeliculaEditar);
 req.body.anio = Number(req.body.anio)
 pelicula.titulo = req.body.titulo
 pelicula.director = req.body.director
 pelicula.anio = req.body.anio
+await guardarPeliculas();
 res.redirect('/');
 });
 
-app.delete('/eliminar-pelicula', (req, res) => {
+app.delete('/eliminar-pelicula', async (req, res) => {
   const idEliminar = Number(req.query.id);
   peliculas = peliculas.filter(p => p.id !== idEliminar);
+  await guardarPeliculas();
   res.json(peliculas);
 });
 app.listen(PORT, () => {
